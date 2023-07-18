b1ec7d3c1d9a06d80f1f6edbc60e41f1dee3032a
Mevlüt Can Tuna <mttuna90@gmail.com>
2023-07-18 17:58:19 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import cors from 'cors'
+import express from 'express' 
+import { PrismaClient } from '@prisma/client'
+import dotenv from "dotenv";
+dotenv.config();
+
+const prisma = new PrismaClient()
+const app = express()
+app.use(cors())
+app.use(express.json()); // For parsing JSON data
+app.use(express.urlencoded({ extended: true })); // For parsing URL-encoded data
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send('Hello World!23')
+})   
+
+app.post('/create-user', async (req: express.Request, res: express.Response) => {
+  const { fullName, email, password} = req.body
+  console.log(req.body)
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        fullName: fullName,
+        email: email,
+        password:password
+      },
+    })
+    res.status(201).json(newUser)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'Something went wrong!' });
+  }
+
+})
+ 
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
