6927e5e59467ebd1d20052945097894216238da0
Mevlüt Can Tuna <mttuna90@gmail.com>
2023-07-19 23:25:30 +0300

    updated

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,8 +3,8 @@ import { getUser, login, signup } from "../controllers/auth.ts";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
+//router.post("/signup", signup);
+//router.post("/login", login);
 //router.get("/user", getUser);
 
 export default router;
