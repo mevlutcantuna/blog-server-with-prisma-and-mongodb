20787a594838490dd0a7ec3c1ac5809f1597f548
Mevlüt Can Tuna <mttuna90@gmail.com>
2023-07-20 00:54:29 +0300

    auth routes changed

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-//import { getUser, login, signup } from "../controllers/auth.ts";
+import { getUser, login, signup } from "../controllers/auth.js";
 
 const router = express.Router();
 
@@ -7,8 +7,8 @@ router.get("/deneme", (req, res) => {
     res.send("Blog Prisma - MongoDB Server Deneme!");
 });
 
-//router.post("/signup", signup);
-//router.post("/login", login);
-//router.get("/user", getUser);
+router.post("/signup", signup);
+router.post("/login", login);
+router.get("/user", getUser);
 
 export default router;
