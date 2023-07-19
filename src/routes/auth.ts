f5012e8d5753c9c7c7680fcb9b1433012b5733f0
Mevlüt Can Tuna <mttuna90@gmail.com>
2023-07-20 00:16:51 +0300

    updated 2

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,8 +3,12 @@ import { getUser, login, signup } from "../controllers/auth.ts";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
-router.get("/user", getUser);
+router.get("/22", (req, res) => {
+    res.send("Blog Prisma - MongoDB Server 222!");
+});
+
+//router.post("/signup", signup);
+//router.post("/login", login);
+//router.get("/user", getUser);
 
 export default router;
