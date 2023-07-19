60b6e29836dbc19bdbbc0b65bd8cd2127ed256ea
Mevlüt Can Tuna <mttuna90@gmail.com>
2023-07-20 00:26:48 +0300

    example

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import cors from "cors";
 import express from "express";
 import router from "./routes/auth.ts";
 import { signup } from "./controllers/auth.ts";
-
+import { PrismaClient } from "@prisma/client";
+import { generateHashedPassword } from "./utils/index.ts";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const prisma = new PrismaClient();
 
 app.get("/", (req, res) => {
     res.send("Blog Prisma - MongoDB Server!");
@@ -18,7 +20,22 @@ app.get("/deneme", (req, res) => {
     res.send("Blog Prisma - MongoDB Server! deneme");
 });
 
-app.post("/signup", signup);
+app.post("/signup", async (req: express.Request, res: express.Response) => {
+    const { fullName, email, password } = req.body;
+
+    try {
+        const newUser = await prisma.user.create({
+            data: {
+                fullName: fullName,
+                email: email,
+                password: generateHashedPassword(password),
+            },
+        });
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(500).json({ error: "Something went wrong!" });
+    }
+});
 
 //app.use(router);
 
