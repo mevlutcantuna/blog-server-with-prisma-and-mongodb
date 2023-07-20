a641269d0cfdbfcc55d366088af16f147c4b8a1e
Mevlüt Can Tuna <mttuna90@gmail.com>
2023-07-20 09:03:00 +0300

    ts to js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
-import router from "./routes/auth.ts";
+import router from "./routes/auth.js";
 const app = express();
 
 app.use(cors());
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUser, login, signup } from "../controllers/auth.ts";
+import { getUser, login, signup } from "../controllers/auth.js";
 
 const router = express.Router();
 
